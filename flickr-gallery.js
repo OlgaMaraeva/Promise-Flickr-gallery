15f6871ee8c3a43cf1782985852440e454f134eb
OlgaMaraeva <OlgaMaraeva@users.noreply.github.com>
2016-01-03 02:55:25 +0400

    Create flickr-gallery.js

---
+++ flickr-gallery.js
@@ -0,0 +1,30 @@
+ $.ajax({
+   url: 'http://api.flickr.com/services/feeds/groups_pool.gne?id=807213@N20&lang=en-us&format=json&jsoncallback=?',
+   dataType: 'jsonp'
+ }).then(data => {
+   var items = data.items;
+   items.forEach((el) => {
+     var img = new Image();
+     img.src = el.media.m;
+     img.className = "pics";
+     setTimeout(() => {
+       $('#content').append(img);
+     }, 1000);
+   });
+ });
+ 
+ /*
+ <div id="content"></div>
+ .pics {
+  max-width:100px;
+  height:auto;
+  border:2px solid #fff;
+}
+
+#content {
+  max-width:600px;
+  display:block;
+  margin:0 auto;
+}
+
+ */
